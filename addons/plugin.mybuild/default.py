8d96e89a4aa22968ac8e175d1f5f0f3482eb4783
gizmo0075 <89253368+gizmo0075@users.noreply.github.com>
2025-12-02 16:09:50 +0000

    kodi build
    trying third time from scratch

---
+++ addons/plugin.mybuild/default.py
@@ -0,0 +1,17 @@
+﻿import sys
+import xbmcplugin, xbmcgui, xbmcaddon
+
+addon = xbmcaddon.Addon()
+addon_name = addon.getAddonInfo('name')
+
+xbmcplugin.setContent(int(sys.argv[1]), 'videos')
+
+# Create a list item
+url = 'https://example.com/video.mp4'
+li = xbmcgui.ListItem(label='Test Video')
+li.setInfo('video', {'title': 'Test Video'})
+li.setProperty('IsPlayable', 'true')
+
+# Add item to directory
+xbmcplugin.addDirectoryItem(int(sys.argv[1]), url, li, isFolder=False)
+xbmcplugin.endOfDirectory(int(sys.argv[1]))
